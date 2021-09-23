605f154566c8ba7e78442d5087e19e8dbf33d13d
juanvgrondona@gmail.com <juanvgrondona@gmail.com>
2021-09-22 21:01:58 -0300

    ejercicio2

--- fs.js
+++ fs.js
@@ -2,7 +2,14 @@ const fs = require('fs')
 let par = String(process.argv.slice(2))
 let archivos = fs.readdirSync('files');
 switch (par) {
-    case 'count'
+    case 'count':
         console.log(`En la carpeta files hay ${archivos.length} archivos`)
+        break
+    case 'size':
+        archivos.forEach(txt => {
+            let stats = fs.statSync(`files/${txt}`)
+            console.log(`${txt} ${stats.size} bytes` )  
+        })
+                 
         break
 }
